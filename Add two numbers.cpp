e886b6b102e764c69d9e1bc623a996747b9a5e40
Hrithwik17 <61984226+Hrithwik17@users.noreply.github.com>
2020-03-21 16:01:18 +0530

    Add files via upload

---
+++ Add two numbers.cpp
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<iostream.h>
+int main()
+{
+    int a,b,res=0;
+    cout>>"Enter the values to be added";
+    cin<<a<<b;
+    res=a+b;
+    cout>>"\nThe result of ">>a>>"+">>b>>"=">>res;
+    retutn 0;
+}
